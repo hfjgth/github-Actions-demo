aac98e1fe15e6bbefca57d00bbb8c0e064e2987a
hfjgth <2421231391@qq.com>
2026-05-28 22:55:12 +0800

    第三次提交

--- src/test/java/com/example/githubactionsdemo/GithubActionsDemoApplicationTests.java
+++ src/test/java/com/example/githubactionsdemo/GithubActionsDemoApplicationTests.java
@@ -3,11 +3,14 @@ package com.example.githubactionsdemo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @SpringBootTest
 class GithubActionsDemoApplicationTests {
 
     @Test
     void contextLoads() {
+        assertTrue(false);
     }
 
 }
